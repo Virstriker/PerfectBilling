f70fe7bf579bf9b29220a4eeb0a4ca57fee161dd
vinayak  gohel <104017167+Virstriker@users.noreply.github.com>
2024-10-26 19:56:09 +0530

    123

--- PerfectDesignerHubDatabaseSchema.sql
+++ PerfectDesignerHubDatabaseSchema.sql
@@ -1,46 +1,44 @@
 CREATE TABLE Admin (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     Name VARCHAR(255),
     Surname VARCHAR(255),
     PhoneNumber BIGINT,
-    UserId VARCHAR(255),
+    UserId BIGINT,
     UserPassword VARCHAR(255),
     Branch VARCHAR(255)
 );
 
 CREATE TABLE CustomerTable (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     Name VARCHAR(255),
     Surname VARCHAR(255),
     PhoneNumber BIGINT,
     Address VARCHAR(255)
 );
-
+a
 CREATE TABLE OrderTable (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     CustomerId BIGINT,
     BranchName VARCHAR(255),
     OrderTables VARCHAR(255),
     OrderDate DATE,
     DeliveryDate DATE,
     OrderStatus BOOLEAN,
-    TotalPrice FLOAT,
     FOREIGN KEY (CustomerId) REFERENCES CustomerTable(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Chaniyo (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     OrderTableId BIGINT,
     Style VARCHAR(255),
     Detail VARCHAR(255),
     ClothImage VARCHAR(255),
     DesingImage VARCHAR(255),
-    Price FLOAT,
     FOREIGN KEY (OrderTableId) REFERENCES OrderTable(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Blouse (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     OrderTableId BIGINT,
     Style VARCHAR(255),
     NeckDesign VARCHAR(255),
@@ -52,12 +50,11 @@ CREATE TABLE Blouse (
     Detail VARCHAR(255),
     ClothImage VARCHAR(255),
     DesingImage VARCHAR(255),
-    Price FLOAT,
     FOREIGN KEY (OrderTableId) REFERENCES OrderTable(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Dress (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     OrderTableId BIGINT,
     Style VARCHAR(255),
     NeckDesign VARCHAR(255),
@@ -69,56 +66,78 @@ CREATE TABLE Dress (
     Detail VARCHAR(255),
     ClothImage VARCHAR(255),
     DesingImage VARCHAR(255),
-    Price FLOAT,
     FOREIGN KEY (OrderTableId) REFERENCES OrderTable(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Pant (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     OrderTableId BIGINT,
     Rubbert VARCHAR(255),
     NeckDesign VARCHAR(255),
     Pocket BOOLEAN,
     Detail VARCHAR(255),
-    Price FLOAT,
     FOREIGN KEY (OrderTableId) REFERENCES OrderTable(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Measurement (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     CustomerId BIGINT,
-    Sholder FLOAT,
-    SholderSlope FLOAT,
-    NeckRound FLOAT,
-    FrontNeckDeep FLOAT,
-    BackNeckDeep FLOAT,
-    AcrossFront FLOAT,
-    AcrossBack FLOAT,
-    Chest FLOAT,
-    Bust FLOAT,
-    Weist FLOAT,
-    Heap FLOAT,
-    ArmHoldLeft FLOAT,
-    ArmHoldRight FLOAT,
-    BicepLeft FLOAT,
-    BicepRight FLOAT,
-    Mori FLOAT,
-    SleeveLength FLOAT,
-    ApexPointLength FLOAT,
-    BlouseLength FLOAT,
-    SideCutLength FLOAT,
-    DressLength FLOAT,
-    OnepieceLength FLOAT,
-    PantWeist FLOAT,
-    PantHeap FLOAT,
-    PantCroch FLOAT,
-    PantJang FLOAT,
-    PantKnee FLOAT,
-    PantAnkel FLOAT,
-    PantMori FLOAT,
-    PantLength FLOAT,
-    ChaniyaWrist FLOAT,
-    ChaniyaHeap FLOAT,
-    ChaniyaLength FLOAT,
+    Sholder VARCHAR(255),
+    SholderSlope VARCHAR(255),
+    NeckRound VARCHAR(255),
+    FrontNeckDeep VARCHAR(255),
+    BackNeckDeep VARCHAR(255),
+    AcrossFront VARCHAR(255),
+    AcrossBack VARCHAR(255),
+    Chest VARCHAR(255),
+    Bust VARCHAR(255),
+    Weist VARCHAR(255),
+    Heap VARCHAR(255),
+    ArmHoldLeft VARCHAR(255),
+    ArmHoldRight VARCHAR(255),
+    BicepLeft VARCHAR(255),
+    BicepRight VARCHAR(255),
+    Mori VARCHAR(255),
+    SleeveLength VARCHAR(255),
+    ApexPointLength VARCHAR(255),
+    BlouseLength VARCHAR(255),
+    SideCutLength VARCHAR(255),
+    DressLength VARCHAR(255),
+    OnepieceLength VARCHAR(255),
+    PantWeist VARCHAR(255),
+    PantHeap VARCHAR(255),
+    PantCroch VARCHAR(255),
+    PantJang VARCHAR(255),
+    PantKnee VARCHAR(255),
+    PantAnkel VARCHAR(255),
+    PantMori VARCHAR(255),
+    PantLength VARCHAR(255),
+    ChaniyaWrist VARCHAR(255),
+    ChaniyaHeap VARCHAR(255),
+    ChaniyaLength VARCHAR(255),
     FOREIGN KEY (CustomerId) REFERENCES CustomerTable(id) ON DELETE CASCADE
 );
+
+CREATE TABLE Employee (
+    id SERIAL PRIMARY KEY,
+    EmployeeName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE EmployeeWork (
+    id SERIAL PRIMARY KEY,
+    EmployeeId INT NOT NULL,
+    WorkDate DATE NOT NULL,
+    WorkAmount BIGINT NOT NULL,
+    WorkDetail VARCHAR(255),
+    FOREIGN KEY (EmployeeId) REFERENCES Employee(id) ON DELETE CASCADE
+);
+
+CREATE TABLE DailyOrders(
+    id SERIAL PRIMARY KEY,
+    CustomerId BIGINT,
+    OrderAmount BIGINT,
+    OrderDetail VARCHAR(255),
+    OrderState INT,
+    OrderDate DATE,
+    FOREIGN KEY (CustomerId) REFERENCES CustomerTable(id) ON DELETE CASCADE
+)
